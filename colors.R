29f657befb50f2bc3baa762d20f019e4cb5e5aec
bnasr <bijan.s.nasr@gmail.com>
2018-10-17 16:32:05 -0700

    colors

---
+++ colors.R
@@ -0,0 +1,25 @@
+colList.orangePurple <- c('#e66101', '#EEAC7C', '#f7f7f7', '#AA99C8', '#5e3c99')
+colList.brownGreen   <- c('#a6611a', '#dfc27d', '#f5f5f5', '#80cdc1', '#018571')
+colList.redGreen <- c("#D73027", "#FDB671", "#FFFFBF", "#B4DF75", "#1A9850")
+colList.brownGreyGreen <- c('#8c510a', '#d8b365', '#d5d5d5', '#5ab4ac', '#01665e')
+
+colList.purpleOrange <- rev(colList.orangePurple)
+colList.greenBrown <- rev(colList.brownGreen)
+colList.greenRed <- rev(colList.redGreen)
+colList.greenGreyBrown <- rev(colList.brownGreyGreen)
+
+colList <- colList.purpleOrange
+
+colList.orangeGreyPurple <- colorRampPalette(colList.orangePurple)(13)
+colList.orangeGreyPurple[7] <- 'grey'
+
+colList.SurfAndTurf <- c('#20948B','#6AB187','#f7f7f7','#F4CC70','#DE7A22')
+colList.SereneAndSpaLike <- c('#ED5752','#E2DFA2','#92AAC7')
+colList.FunAndTropical <- c('#FA6E59','#F8A055','#FFDB5C','#92AAC7','#4897D8')
+colList.WarmAndRustic <- c('#805A3B','#C60000','#FD974F','#FEF2E4')
+colList.DistinctiveAndUnexpected <- c('#52908B','#E5E2CA','#E7472E')
+colList.GiantGoldFish <- c('#6ed3e7','#a9dcdb','#e1e5ce','#e1e5ce','#f86a20')
+colList.El.Designo <- c('#2b2303','#e3d897','#a79e68','#76afb6','#619ea1')
+colList.Contad <- c('#ccc533','#ffe633','#ee5c33','#b80f6e','#390855')
+
+colList.Ideas4Homes <- c('#6ed3e7','#a9dcdb','#e1e5ce','#f2863c','#f86a20')
