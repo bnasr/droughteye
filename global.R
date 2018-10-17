e3550b329d23d2eb38be0c3efff64f1c057017c1
bnasr <bijan.s.nasr@gmail.com>
2018-10-17 13:11:17 -0700

    global

---
+++ global.R
@@ -0,0 +1,24 @@
+list.of.packages <- c(
+  'rgdal',
+  'shiny',
+  'shinyjs',
+  'shinyBS',
+  'shinyAce',
+  'shinyTime',
+  'shinyFiles',
+  'shinydashboard',
+  'shinythemes',
+  'rjson',
+  'stringr',
+  'sp',
+  'raster',
+  'data.table',
+  'lubridate',
+  'plotly',
+  'RCurl'
+)
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos='http://cran.rstudio.com/')
+
+for(p in list.of.packages) library(p, character.only = T)
