7aeb52a959abe1d6ccd1c979f64bd6a9a084bba7
bnasr <bijan.s.nasr@gmail.com>
2018-10-17 13:11:43 -0700

    update modis

---
+++ update_modis.R
@@ -0,0 +1,17 @@
+source('required_libs.R')
+source('env_vars.R')
+source('funcs.R')
+
+
+this_month <- month(Sys.Date())
+this_year <- year(Sys.Date())
+
+for(y in 2001:this_year)
+  {
+  for(m in 1:12){
+    if(y==this_year&m>=this_month) next()
+    cat('getting MODIS for', y, m, '\n')
+    get_MOD11C3(year = y, month = m, modis_repo = modis_repo)
+  }
+}
+
