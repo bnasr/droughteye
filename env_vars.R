91e6c3b798d259ecc153b5d350b235fe1cc9096a
bnasr <bijan.s.nasr@gmail.com>
2018-10-17 13:41:01 -0700

    updated repos

--- env_vars.R
+++ env_vars.R
@@ -1,3 +1,5 @@
 prism_repo <- '/mnt/monsoon/datarepo/DROUGHT_EYE/PRISM/tmean/'
 modis_repo <- '/mnt/monsoon/datarepo/DROUGHT_EYE/MODIS/MOD11C3/'
 mcd11c3_web_repo <- 'https://e4ftl01.cr.usgs.gov/MOLT/MOD11C3.006/'
+
+deltat_repo <- '/mnt/monsoon/datarepo/DROUGHT_EYE/DELTA_T/'
