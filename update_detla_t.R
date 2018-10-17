4b7cfe244c94715a30d1f480710e13a52e113eff
bnasr <bijan.s.nasr@gmail.com>
2018-10-17 13:42:38 -0700

    update delta T

---
+++ update_detla_t.R
@@ -0,0 +1,15 @@
+source('required_libs.R')
+source('env_vars.R')
+source('funcs.R')
+
+this_month <- month(Sys.Date())
+this_year <- year(Sys.Date())
+
+for(y in 2001:this_year)
+{
+  for(m in 1:12){
+    if(y==this_year&m>=this_month) next()
+    cat('writing deltaT for', y, m, '\n')
+    write_detla_t(y, m, deltat_repo)
+  }
+}
