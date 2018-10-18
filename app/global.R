26e03a4b80e18b8dde94b448fe16b20b0db21b9e
bnasr <bijan.s.nasr@gmail.com>
2018-10-17 21:22:04 -0700

    ui

--- app/global.R
+++ app/global.R
@@ -17,6 +17,7 @@ list.of.packages <- c(
 )
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
 if(length(new.packages)) install.packages(new.packages, repos='http://cran.rstudio.com/')
 
 for(p in list.of.packages) library(p, character.only = T)
@@ -24,4 +25,3 @@ for(p in list.of.packages) library(p, character.only = T)
 source('~/Projects/droughteye/env_vars.R')
 source('~/Projects/droughteye/funcs.R')
 source('~/Projects/droughteye/colors.R')
-
