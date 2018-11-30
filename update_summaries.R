be9f1d8427a81e93cb0954400111d16ac610e610
bnasr <bijan.s.nasr@gmail.com>
2018-11-30 13:08:18 -0700

    summ

--- update_summaries.R
+++ update_summaries.R
@@ -7,16 +7,35 @@ source('env_vars.R')
 source('funcs.R')
 
 
+phys <- raster::shapefile(paste0(data_repo, 'physio/physio.shp'))
+
 this_month <- month(Sys.Date())
 this_year <- year(Sys.Date())
 
 for(y in 2001:this_year){
   for(m in 1:12){
     if(y==this_year&m>=this_month) next()
-    
-    cat('getting summaries for', y, m, '\n')
-    get_MOD11C3(year = y, month = m, modis_repo = modis_repo)
+    for(type in c('delta', 'normal', 'anomaly')){
+      cat('getting summaries for', type, y, m, '\n')
+      path <- switch (type,
+                      'delta' = sprintf(fmt = '%sDELTA/DELTAT.%04d.%02d.01.tif', deltat_repo, y, m),
+                      'normal' = sprintf(fmt = '%sNORMAL/DELTAT.NORMAL.%02d.01.tif', deltat_repo, m),
+                      'anomaly' = sprintf(fmt = '%sANOMALY/DELTAT.ANOMALY.%04d.%02d.01.tif', deltat_repo, y, m)
+      )
+      
+      summ_path <- sprintf(fmt = '%sSUMM.%s.%04d.%02d.01.rds', summ_repo, toupper(type), y, m)
+      
+      if(file.exists(summ_path)){
+        cat(summ_path, ' already exists!\n')
+        next()
+      }
+      
+      map_raster <- raster(path)
+      zonal_stats <- raster::extract(map_raster, phys)
+      names(zonal_stats) <- tools::toTitleCase(tolower(phys$PROVINCE))
+      saveRDS(zonal_stats, file = summ_path)
+      cat(Sys.time(), '\t summaries done!\n')
+    }
   }
 }
 
-cat(Sys.time(), '\t summaries done!\n')
