b552da3a24ccf5c67fd4b6a4d0301d995914d8bd
bnasr <bijan.s.nasr@gmail.com>
2018-11-30 19:21:48 -0700

    update

--- update_summaries.R
+++ update_summaries.R
@@ -1,7 +1,6 @@
 cat('\n\n=============================================================================================\n')
 
 cat(Sys.time(), '\t updating summaries ...\n')
-
 source('required_libs.R')
 source('env_vars.R')
 source('funcs.R')
@@ -11,6 +10,8 @@ phys <- raster::shapefile(paste0(data_repo, 'physio/physio.shp'))
 
 this_month <- month(Sys.Date())
 this_year <- year(Sys.Date())
+
+summDT <- data.table()
 
 for(y in 2001:this_year){
   for(m in 1:12){
@@ -26,16 +27,26 @@ for(y in 2001:this_year){
       summ_path <- sprintf(fmt = '%sSUMM.%s.%04d.%02d.01.rds', summ_repo, toupper(type), y, m)
       
       if(file.exists(summ_path)){
+        zonal_stats <- readRDS(summ_path)
         cat(summ_path, ' already exists!\n')
-        next()
+      }else{
+        map_raster <- raster(path)
+        zonal_stats <- raster::extract(map_raster, phys)
+        names(zonal_stats) <- tools::toTitleCase(tolower(phys$PROVINCE))
+        saveRDS(zonal_stats, file = summ_path)
       }
+      quant <- t(sapply(zonal_stats, quantile, na.rm = TRUE, probs = c(0.025, 0.25, 0.50, 0.75, 0.975)))
+      qsd <- sapply(zonal_stats, sd, na.rm = TRUE)
+      qmean <- sapply(zonal_stats, mean, na.rm = TRUE)
       
-      map_raster <- raster(path)
-      zonal_stats <- raster::extract(map_raster, phys)
-      names(zonal_stats) <- tools::toTitleCase(tolower(phys$PROVINCE))
-      saveRDS(zonal_stats, file = summ_path)
-      cat(Sys.time(), '\t summaries done!\n')
+      quantDT <- data.table(year = y, month = m, regions = rownames(quant), mean = qmean, sd = qsd, quant)
+      quantDT <- melt(quantDT, id.vars = 1:3)
+      
+      summDT <- rbind(summDT, quantDT)
     }
   }
 }
 
+saveRDS(file = sprintf(fmt = '%sSUMM.ALL.rds', summ_repo), object = summDT)
+
+cat(Sys.time(), '\t summaries done!\n')
