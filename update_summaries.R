d4d088439c9e2c8cad71a971da4f10c4df70b25b
bnasr <bijan.s.nasr@gmail.com>
2018-11-30 20:28:22 -0700

    temporal

--- update_summaries.R
+++ update_summaries.R
@@ -16,12 +16,12 @@ summDT <- data.table()
 for(y in 2001:this_year){
   for(m in 1:12){
     if(y==this_year&m>=this_month) next()
-    for(type in c('delta', 'normal', 'temporal')){
+    for(type in c('temporal', 'normal', 'anomaly')){
       cat('getting summaries for', type, y, m, '\n')
       path <- switch (type,
-                      'delta' = sprintf(fmt = '%sDELTA/DELTAT.%04d.%02d.01.tif', deltat_repo, y, m),
+                      'temporal' = sprintf(fmt = '%sDELTA/DELTAT.TEMPORAL.%04d.%02d.01.tif', deltat_repo, y, m),
                       'normal' = sprintf(fmt = '%sNORMAL/DELTAT.NORMAL.%02d.01.tif', deltat_repo, m),
-                      'temporal' = sprintf(fmt = '%sANOMALY/DELTAT.ANOMALY.%04d.%02d.01.tif', deltat_repo, y, m)
+                      'anomaly' = sprintf(fmt = '%sANOMALY/DELTAT.ANOMALY.%04d.%02d.01.tif', deltat_repo, y, m)
       )
       
       summ_path <- sprintf(fmt = '%sSUMM.%s.%04d.%02d.01.rds', summ_repo, toupper(type), y, m)
