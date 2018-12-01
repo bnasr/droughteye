69bced4ca9500f9beaaa703c8895cd222f257661
bnasr <bijan.s.nasr@gmail.com>
2018-11-30 20:20:22 -0700

    ui

--- app/ui.R
+++ app/ui.R
@@ -50,57 +50,59 @@ fluidPage(
     ),
     
     mainPanel = mainPanel(width = 10,
-                          tabsetPanel(
-                            
-                            tabPanel('Thermal Stress Map',
-                                     
-                                     br(),
-                                     radioButtons('month', 
-                                                  label = NULL, 
-                                                  inline = TRUE,
-                                                  choices = month.name,
-                                                  selected = month.name[max(1, month(Sys.Date())-1)]
-                                     ),
-                                     
-                                     plotOutput('map_plot', 
-                                                width = '100%', 
-                                                click = 'map_click', 
-                                                dblclick = 'map_dblclick', 
-                                                hover = 'map_hover')
-                            ),
-                            
-                            tabPanel('Zonal Statistics', 
-                                     br(),
-                                     # actionButton('update_zonal', 
-                                     #              label = HTML('<strong style="color:#ff0000;">Zonal stats are not up-to-date. Click here to update</strong>'),
-                                     #              icon = icon('redo'), width = '100%'),
-                                     hr(),
-                                     plotOutput('zonal_plot', 
-                                                width = '100%')
-                            ),
-                            
-                            tabPanel('Physiographic Map', 
-                                     br(),
-                                     br(),
-                                     br(),
-                                     plotOutput('physio_plot', 
-                                                width = '100%',
-                                                hover = 'physio_hover')
-                            ),
-                            
-                            tabPanel('Percent Tree Cover', 
-                                     br(),
-                                     br(),
-                                     br(),
-                                     plotOutput('tree_percent_plot', 
-                                                width = '100%',
-                                                hover = 'tree_percent_hover')
-                            ),
-                            
-                            tabPanel('About',{
-                              includeHTML('about.html')
-                            })
-                          )
+                          {
+
+                            tabsetPanel(
+                              radioButtons('month', 
+                                           label = NULL, 
+                                           inline = TRUE,
+                                           choices = month.name,
+                                           selected = month.name[max(1, month(Sys.Date())-1)]
+                              ),                              
+                              tabPanel('Thermal Stress Map',
+                                       
+                                       br(),
+                                       
+                                       plotOutput('map_plot', 
+                                                  width = '100%', 
+                                                  click = 'map_click', 
+                                                  dblclick = 'map_dblclick', 
+                                                  hover = 'map_hover')
+                              ),
+                              
+                              tabPanel('Zonal Statistics', 
+                                       br(),
+                                       # actionButton('update_zonal', 
+                                       #              label = HTML('<strong style="color:#ff0000;">Zonal stats are not up-to-date. Click here to update</strong>'),
+                                       #              icon = icon('redo'), width = '100%'),
+                                       hr(),
+                                       plotOutput('zonal_plot', 
+                                                  width = '100%')
+                              ),
+                              
+                              tabPanel('Physiographic Map', 
+                                       br(),
+                                       br(),
+                                       br(),
+                                       plotOutput('physio_plot', 
+                                                  width = '100%',
+                                                  hover = 'physio_hover')
+                              ),
+                              
+                              tabPanel('Percent Tree Cover', 
+                                       br(),
+                                       br(),
+                                       br(),
+                                       plotOutput('tree_percent_plot', 
+                                                  width = '100%',
+                                                  hover = 'tree_percent_hover')
+                              ),
+                              
+                              tabPanel('About',{
+                                includeHTML('about.html')
+                              })
+                            )
+                          }
                           
     )
   )
